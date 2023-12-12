56e6beedf5aa728e0fbdaa8a81114bf51ccfab8c
Avaz <elonarm21@gmail.com>
2023-12-12 12:14:06 +0500

    redux working

--- src/index.js
+++ src/index.js
@@ -1,17 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "bootstrap/dist/css/bootstrap.css"
+// Redux da store bo'limini yaratish
+import {createStore} from "redux"
 
+const initialState = 0
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case "inc":
+            return state + 1
+        case "dec":
+            return state - 1
+        case "res":
+            return 0
+        case "random":
+            return action.payload
+        default:
+            return state
+    }
+}
+
+const store = createStore(reducer)
+store.subscribe(() => {
+    // console.log("store", store.getState())
+    document.getElementById("count").textContent = store.getState()
+})
+document.getElementById("inc").addEventListener("click", () => {
+    store.dispatch({type: "inc"})
+})
+
+document.getElementById("res").addEventListener("click", () => {
+    store.dispatch({type: "res"})
+})
+
+document.getElementById("dec").addEventListener("click", () => {
+    store.dispatch({type: "dec"})
+})
+
+document.getElementById("random").addEventListener("click", () => {
+    let payload = Math.floor(Math.random() * 100)
+    store.dispatch({type: "random", payload:payload})
+})
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <></>
+    </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
